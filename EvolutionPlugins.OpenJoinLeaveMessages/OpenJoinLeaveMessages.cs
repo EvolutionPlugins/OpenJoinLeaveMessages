4689db374a43534987e5434d2c80fdaac6b0643d
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-06-21 19:24:48 +0700

    Change plugin Id

--- EvolutionPlugins.OpenJoinLeaveMessages/OpenJoinLeaveMessages.cs
+++ EvolutionPlugins.OpenJoinLeaveMessages/OpenJoinLeaveMessages.cs
@@ -2,7 +2,7 @@
 using OpenMod.Core.Plugins;
 using System;
 
-[assembly: PluginMetadata("OpenJoinLeaveMessages", Author = "EvolutionPlugins", DisplayName = "Open Join & Leave Messages",
+[assembly: PluginMetadata("EvolutionPlugins.OpenJoinLeaveMessages", Author = "EvolutionPlugins", DisplayName = "Open Join & Leave Messages",
     Website = "https://discord.gg/5MT2yke")]
 
 namespace EvolutionPlugins.OpenJoinLeaveMessages
